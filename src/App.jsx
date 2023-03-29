de77020add7c99beb758a7f7cf9d3b267c33f82c
AngeloHoeung <angelohoeung@gmail.com>
2023-03-28 23:08:21 -0400

    Added github repos Still need to add icons for languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,42 @@
-import { useState } from 'react'
-import './App.css'
-import emailLogo from './assets/email.svg'
-import githubLogo from './assets/github.svg'
-import linkedinLogo from './assets/linkedin.svg'
+import { useState } from "react";
+import "./App.css";
+import GitHubRepos from "./components/GitHubRepos";
+import emailLogo from "./assets/email.svg";
+import githubLogo from "./assets/github.svg";
+import linkedinLogo from "./assets/linkedin.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div>
         <a href="mailto:hoeung@uwindsor.ca" title="Email" target="_blank">
-          <img src={emailLogo} className="logo email" alt="Email logo"/>
+          <img src={emailLogo} className="logo email" alt="Email logo" />
         </a>
-        <a href="https://github.com/angelohoeung/" title="GitHub" target="_blank">
-          <img src={githubLogo} className="logo github" alt="GitHub logo"/>
+        <a
+          href="https://github.com/angelohoeung/"
+          title="GitHub"
+          target="_blank"
+        >
+          <img src={githubLogo} className="logo github" alt="GitHub logo" />
         </a>
-        <a href="https://www.linkedin.com/in/angelohoeung/" title="LinkedIn" target="_blank">
-          <img src={linkedinLogo} className="logo linkedin" alt="LinkedIn logo"/>
+        <a
+          href="https://www.linkedin.com/in/angelohoeung/"
+          title="LinkedIn"
+          target="_blank"
+        >
+          <img
+            src={linkedinLogo}
+            className="logo linkedin"
+            alt="LinkedIn logo"
+          />
         </a>
       </div>
       <h1>Angelo Hoeung</h1>
       <p>Hi, this is my personal website made in React.js using Vite.</p>
+      <h2>Projects</h2>
+      <GitHubRepos username="angelohoeung" />
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -34,7 +49,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/GitHubRepos.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../App.css";
+
+const GitHubRepos = ({ username }) => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${username}/repos?sort=updated`)
+      .then((response) => {
+        const reposWithAboutAndCommits = response.data.map((repo) => {
+          // fetch the repository information
+          axios
+            .get(`https://api.github.com/repos/${username}/${repo.name}`)
+            .then((response) => {
+              const about = response.data.description;
+              // set the repository information along with the 'about' field
+              setRepos((prevRepos) => {
+                return prevRepos.map((r) => {
+                  if (r.id === repo.id) {
+                    return { ...r, about };
+                  }
+                  return r;
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          // fetch the date of the latest commit for the repository
+          axios
+            .get(
+              `https://api.github.com/repos/${username}/${repo.name}/commits`
+            )
+            .then((response) => {
+              const latestCommitDate =
+                response.data.length > 0
+                  ? new Date(
+                      response.data[0].commit.author.date
+                    ).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                    })
+                  : "No commits yet";
+              // set the repository information along with the 'latestCommitDate' field
+              setRepos((prevRepos) => {
+                return prevRepos.map((r) => {
+                  if (r.id === repo.id) {
+                    return { ...r, latestCommitDate };
+                  }
+                  return r;
+                });
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          // return the repository object with null values for 'about' and 'latestCommitDate'
+          return { ...repo, about: null, latestCommitDate: null };
+        });
+        setRepos(reposWithAboutAndCommits);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [username]);
+
+  return (
+    <div className="repos-grid">
+      {repos.slice(0, 6).map((repo) => (
+        <div key={repo.id} className="repo-item">
+          <h3>
+            <a href={repo.html_url} target="_blank">
+              {repo.name}
+            </a>
+            {repo.language && <span className="language-icon" data-language={repo.language}> {repo.language}</span>}
+          </h3>
+          <p>{repo.description}</p>
+          {/* <p>Language: {repo.language}</p> */}
+          <p>Latest Commit: {repo.latestCommitDate || "No commits yet"}</p>
+          {/* {repo.about && <p>About: {repo.about}</p>} */}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default GitHubRepos;
